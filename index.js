2042f31e81aa32cfa6fd81da26777f71203f7503
Pierre Lanneau <26084911SlipNinja@users.noreply.github.com>
2023-06-04 16:59:44 +0200

    Clean code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { Tree } from "./binary_search_tree.js";
 
-function displayValue(node){
-    //console.log(node.value);
-}
-
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ( max - min ) + min);
 }
@@ -17,7 +13,6 @@ function getRandomArray(size, min, max){
     return array;
 }
 
-//let myArray = [4, 1, 2, 3, 12, 13, 7, 4, 8, 9, 10, 11, 2, 4, 2, 4];
 let myArray = getRandomArray(20, 1, 100);
 let tree = new Tree(myArray);
 
@@ -37,7 +32,7 @@ tree.rebalance();
 tree.print();
 console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
 
-console.log("Level order : " + tree.levelOrder(displayValue));
-console.log("In order : " + tree.inOrder(displayValue));
-console.log("Pre order : " + tree.preOrder(displayValue));
-console.log("Post order : " + tree.postOrder(displayValue));
+console.log("Level order : " + tree.levelOrder());
+console.log("In order : " + tree.inOrder());
+console.log("Pre order : " + tree.preOrder());
+console.log("Post order : " + tree.postOrder());
