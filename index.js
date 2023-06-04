a5e9b477dbc96e9ecdf29d1a90128bf09680d0fb
Pierre Lanneau <26084911SlipNinja@users.noreply.github.com>
2023-06-04 16:58:15 +0200

    Add test code

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ function displayValue(node){
     //console.log(node.value);
 }
 
-let myArray = [4, 1, 2, 3, 12, 13, 7, 4, 8, 9, 10, 11, 2, 4, 2, 4];
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * ( max - min ) + min);
+}
+
+function getRandomArray(size, min, max){
+    let array = [];
+    while(size > 0){
+        size--;
+        array.push(getRandomNumber(min, max));
+    }
+    return array;
+}
+
+//let myArray = [4, 1, 2, 3, 12, 13, 7, 4, 8, 9, 10, 11, 2, 4, 2, 4];
+let myArray = getRandomArray(20, 1, 100);
 let tree = new Tree(myArray);
 
 console.log(`Generating tree from : ${myArray}`);
 tree.print();
+console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
 
-let value1 = 6;
-let value2 = 18;
-
-console.log(`Inserting ${value1}...`);
-tree.insert(6);
-
-tree.print();
-
-console.log("Deleting root node value...")
-tree.delete(tree.root.value);
+let addValues = getRandomArray(10, 100, 200);
+addValues.forEach(value => {
+    tree.insert(value);
+});
 
 tree.print();
+console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
 
-console.log(tree.find(value1) ? `Value ${value1} is in the tree` : `Value ${value1} not found`);
-console.log(tree.find(value2) ? `Value ${value2} is in the tree` : `Value ${value2} not found`);
+tree.rebalance();
+tree.print();
+console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
 
 console.log("Level order : " + tree.levelOrder(displayValue));
 console.log("In order : " + tree.inOrder(displayValue));
 console.log("Pre order : " + tree.preOrder(displayValue));
 console.log("Post order : " + tree.postOrder(displayValue));
-
-console.log("Height from node '7' : " + tree.height(tree.find(7)));
-console.log("Depth to node '7' : " + tree.depth(tree.find(7)));
-
-console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
-
-tree.rebalance();
-tree.print();
-
-console.log(tree.isBalanced() ? "Tree is balanced" : "Tree is not balanced");
-
